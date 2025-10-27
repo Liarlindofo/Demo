a5baec45b1bc188415fd99240e9c18e49fe06380
Liarlindofo <liar.contato@outlook.com>
2025-10-27 19:35:18 -0300

    fix: mover eslint-disable para antes da função

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,6 +1,6 @@
 import { StackHandler } from '@stackframe/stack';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Handler(props: any) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return <StackHandler fullPage {...props} />;
 }
